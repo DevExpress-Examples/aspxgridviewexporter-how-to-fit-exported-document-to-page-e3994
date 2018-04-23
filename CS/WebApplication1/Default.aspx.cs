12e9ffef5f4b8085000aabff448f5328eefd357e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebApplication1/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using DevExpress.XtraPrinting;
+using System.Net.Mime;
+
+namespace WebApplication1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ASPxButton1_Click(object sender, EventArgs e)
+        {
+            using(MemoryStream ms = new MemoryStream()){
+                PrintableComponentLink pcl = new PrintableComponentLink(new PrintingSystem());
+                pcl.Component = ASPxGridViewExporter1;
+                pcl.Margins.Left = pcl.Margins.Right = 50;
+                pcl.Landscape = true;
+                pcl.CreateDocument(false);
+                pcl.PrintingSystem.Document.AutoFitToPagesWidth = 1;
+                pcl.ExportToPdf(ms);
+                WriteResponse(this.Response, ms.ToArray(), System.Net.Mime.DispositionTypeNames.Inline.ToString());
+            }
+        }
+        public static void WriteResponse(HttpResponse response, byte[] filearray, string type)
+        {
+            response.ClearContent();
+            response.Buffer = true;
+            response.Cache.SetCacheability(HttpCacheability.Private);
+            response.ContentType = "application/pdf";
+            ContentDisposition contentDisposition = new ContentDisposition();
+            contentDisposition.FileName = "test.pdf";
+            contentDisposition.DispositionType = type;
+            response.AddHeader("Content-Disposition", contentDisposition.ToString());
+            response.BinaryWrite(filearray);
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
+            try
+            {
+                response.End();
+            }
+            catch (System.Threading.ThreadAbortException)
+            {
+            }
+
+        }
+
+        protected void ASPxGridView1_CustomSummaryCalculate(object sender, DevExpress.Data.CustomSummaryEventArgs e)
+        {
+
+        }
+
+        protected void ASPxGridView1_HtmlFooterCellPrepared(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableFooterCellEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ASPxGridViewExporter - how to fit exported document to page
+
+
+<p>This example illustrates how to use XtraPrinting's <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraPrintingDocument_AutoFitToPagesWidthtopic"><u>Document.AutoFitToPagesWidth</u></a> property to adjust width of an exported document to occupy a single page.<br />
+This is a temporary workaround for the  <a href="https://www.devexpress.com/Support/Center/p/S130295">ASPxGridViewExporter - FitToPage option</a> request.</p>
+
+<br/>
+
+
